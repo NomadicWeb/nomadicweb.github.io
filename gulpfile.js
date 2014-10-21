6c5418d86daa18d0b7636d8934893482eaf22741
Luke Murphy <lukewm@riseup.net>
2014-10-21 14:48:23 -0200

    add gulpfile watch functionality autoreload
    You also need to install addon at livereload.com

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
-var gulp    = require('gulp');
-var stylus  = require('gulp-stylus');
-var nodemon = require('gulp-nodemon');
-var axis    = require('axis');
+var gulp    = require('gulp'),
+    stylus  = require('gulp-stylus'),
+    nodemon = require('gulp-nodemon'),
+    axis    = require('axis'),
+    reload  = require('gulp-livereload');
 
-gulp.task('stylus', function () {
+
+gulp.task('stylus', function (){
   gulp.src('./app/assets/stylesheets/style.styl')
     .pipe(stylus({error: true, use: [axis()]}))
     .pipe(gulp.dest('./app/assets/stylesheets'));
 });
 
-gulp.task('dev', function() {
-  nodemon({ script: 'server.js', ext: 'jade styl js', ignore: ['./node_modules/**'] })
-    .on('change', ['stylus'])
-    .on('restart', function () {
-      console.log('restarted!');
-    });
+gulp.task('dev', function(){
+  reload.listen();
+  nodemon({ 
+      script: 'server.js', 
+      ext: 'jade styl js', 
+      ignore: ['./node_modules/**'] ,
+  })
+  .on('start', function(){
+    console.log('started!');
+  })
+
+  .on('change', ['stylus', 'watch'])
+
+  .on('restart', function (){
+    console.log('restarted!');
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./**').on('change', reload.changed);
 });
 
 gulp.task('default', ['dev']);
