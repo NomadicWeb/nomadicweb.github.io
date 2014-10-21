2ad2c5bb71276463f4f6fa6b58d90e746b599fdb
Luke Murphy <lukewm@riseup.net>
2014-10-21 20:30:20 -0200

    add basic about luke/martin routes

--- app/router/index.js
+++ app/router/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
-    app.use('/',        require('./routes/index/index.js'));
-    app.use('/about',   require('./routes/about/index.js'));
+    app.use('/', require('./routes/index/index.js'));
+    app.use('/about', require('./routes/about/index.js'));
     app.use('/clients', require('./routes/clients/index.js'));
     app.use('/hire-us', require('./routes/hire-us/index.js'));
 };
--- app/router/routes/about/index.js
+++ app/router/routes/about/index.js
@@ -5,4 +5,12 @@ router.get('/', function (req, res) {
     res.render('about', { title : 'Home' });
 });
 
+router.get('/luke', function (req, res) {
+    res.render('luke', { title : 'Home' });
+});
+
+router.get('/martin', function (req, res) {
+    res.render('martin', { title : 'Home' });
+});
+
 module.exports = router;
