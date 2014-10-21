ffa90b3a7bbe13267793c28d39dd60047b728180
Luke Murphy <lukewm@riseup.net>
2014-10-21 13:35:19 -0200

    add gulpfile config to get autoreload + autocompile stylus
    I also added gulp-nodemon (what a rad module)

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
-var gulp   = require('gulp'),
-    stylus = require('gulp-stylus'),
-    axis   = require('axis')
+var gulp    = require('gulp');
+var stylus  = require('gulp-stylus');
+var nodemon = require('gulp-nodemon');
+var axis    = require('axis');
+
+gulp.task('stylus', function () {
+  gulp.src('./app/assets/stylesheets/style.styl')
+    .pipe(stylus({error: true, use: [axis()]}))
+    .pipe(gulp.dest('./app/assets/stylesheets'));
+});
+
+gulp.task('dev', function() {
+  nodemon({ script: 'server.js', ext: 'jade styl js', ignore: ['./node_modules/**'] })
+    .on('change', ['stylus'])
+    .on('restart', function () {
+      console.log('restarted!');
+    });
+});
+
+gulp.task('default', ['dev']);
