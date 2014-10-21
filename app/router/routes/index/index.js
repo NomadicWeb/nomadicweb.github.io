253fd8658dd41dcfbf54dcbf9a26c4869a393ff4
Luke Murphy <lukewm@riseup.net>
2014-10-21 19:53:32 -0200

    add basic index.jade layout

--- app/router/routes/index/index.js
+++ app/router/routes/index/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('layout', { title : 'Home' });
+    res.render('index', { title : 'Home' });
 });
 
 module.exports = router;
