d6a39aa0e5322536fc97debd7c6565b117594cfb
Luke Murphy <lukewm@riseup.net>
2014-10-29 10:38:17 -0200

    add style for text rotator

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,8 @@
-$("#js-rotating").Morphext({
-    animation: "shake",
+
+// rotator via morphext.js for the front page
+// 'we build `xyz` text
+$("#build-rotator").Morphext({
+    animation: "fadeIn",
     separator: ",",
-    speed: 6000
+    speed: 3500
 });
