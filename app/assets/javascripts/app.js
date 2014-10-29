8b2e7e66228d02d582f3ad29d86f08280334bab4
Luke Murphy <lukewm@riseup.net>
2014-10-29 11:50:23 -0200

    move back the rotator to app.js - jquery not loaded

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,19 @@
+ $("#build-rotator").Morphext({ animation: "fadeIn", separator: ",", speed: 3500 });
+
 $(function() {
   if (Modernizr.history){
     // history is supported; do magical things
+    console.log("Modernizr.history supported!");
+
+    $(".menu").delegate("a", "click", function() {
+      console.log("Hijacking the click event!");
+      var _href = $(this).attr("href");
+      history.pushState(null, null, _href);
+      //loadContent(_href);
+    });
+
   }else{
     // history is not supported; nothing fancy here
+    console.log("Modernizr.history not supported!");
   }
 });
